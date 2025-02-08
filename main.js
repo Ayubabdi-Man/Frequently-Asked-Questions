4ddee84dcf3f09380069dd163003b06f36ba72d7
Ayubaabdi <ayubabdiibraahim@gmail.com>
2025-02-08 10:26:18 +0300

    added js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const BoxOne = document.querySelector('.boxOne');
 const boxtwo = document.querySelector('.boxtwo')
+const boxThree = document.querySelector('.boxThree')
 const answner = document.querySelector('.answner')
 const answnerOne = document.querySelector('.answnerOne')
+const answnerTwo = document.querySelector('.answnerTwo')
 
 BoxOne.addEventListener('click', operation)
 
@@ -20,6 +22,19 @@ function oparationBox() {
     p.textContent = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
     answnerOne.appendChild(p)
 }
+
+
+boxThree.addEventListener('click', boxess)
+
+
+function boxess() {
+    const p = document.createElement('p')
+    p.textContent = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+
+    answnerTwo.appendChild(p)
+}
+
+
 
 
 
